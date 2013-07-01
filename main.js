95938d3c13b32043ea29dc45d835177c9f65567a
Javier Abadia <javier.abadia@esri.es>
2013-07-01 11:50:36 +0200

    limpiando un poco el codigo

--- main.js
+++ main.js
@@ -8,24 +8,24 @@ dojo.require("esri.dijit.InfoWindowLite");
 dojo.require("dojo.date.locale");
 
 var map;
-var dynamicLayer;
-var layerBarcos;
+
+var shipsLayer;
+var shipsFeatureLayer;
 var waiting = false;
 var playing = false;
 
-
-var barcosUrl = "http://79.125.13.101:6080/arcgis/rest/services/GEP_barcos/Barcos_Vigo/MapServer";
-var reservaUrl = "http://79.125.13.101:6080/arcgis/rest/services/GEP_barcos/Reserva_Marina/MapServer";
-var barcosFeatureUrl = "http://79.125.13.101:6080/arcgis/rest/services/GEP_barcos/Barcos_Vigo/FeatureServer";
-var cartaNauticaUrl = "http://79.125.13.101:6080/arcgis/rest/services/s57/RegMar/MapServer/exts/Maritime%20Chart%20Server/MapServer";
+var hostUrl = "http://79.125.13.101:6080/arcgis/rest/services";
+var shipsUrl          = hostUrl + "/GEP_barcos/Barcos_Vigo/MapServer";
+var shipsFeatureUrl   = hostUrl + "/GEP_barcos/Barcos_Vigo/FeatureServer";
+var protectedAreasUrl = hostUrl + "/GEP_barcos/Reserva_Marina/MapServer";
+var nauticalChartUrl  = hostUrl + "/s57/RegMar/MapServer/exts/Maritime%20Chart%20Server/MapServer";
 
 function init()
 {
-
-	esriConfig.defaults.map.panDuration = 500; // time in milliseconds, default panDuration: 250
-	esriConfig.defaults.map.panRate = 1; // default panRate: 25
+	esriConfig.defaults.map.panDuration = 500;	// time in milliseconds, default panDuration: 250
+	esriConfig.defaults.map.panRate = 1; 		// default panRate: 25
 	esriConfig.defaults.map.zoomDuration = 500; // default zoomDuration: 500
-	esriConfig.defaults.map.zoomRate = 1; // default zoomRate: 25
+	esriConfig.defaults.map.zoomRate = 1; 		// default zoomRate: 25
 
 	var options = {
 		basemap: "gray",
@@ -38,32 +38,6 @@ function init()
 	initMap();
 };
 
-function init2()
-{
-	var mapDeferred = esri.arcgis.utils.createMap("f361c51169354eeca8221f3da46c9014","map");
-	mapDeferred.then(function(response)
-	{
-		map = response.map;
-
-		var title = response.itemInfo.item.title;
-		var subtitle = response.itemInfo.item.snippet;
-		console.log(response.itemInfo.item);
-
-		if( map.loaded ) {
-			initMap();
-		}
-		else
-		{
-			dojo.connect(map,"onLoad",function(){
-				initMap();
-			});
-		}
-	},function(error)
-	{
-		console.log("Can't create map: ", dojo.toJson(error),error);
-	});
-
-}
 
 function divideByTen(value,key,data) {	return String(value / 10); }
 function toLower(value,key,data) { return value.toLowerCase(); }
@@ -71,16 +45,19 @@ function toLower(value,key,data) { return value.toLowerCase(); }
 
 function initMap()
 {
-	// capa de carta nautica (S-57)
-	var layerCartaNautica = new esri.layers.ArcGISDynamicMapServiceLayer(cartaNauticaUrl);
+	// -- layers
 
-	// capa de la reserva marina
-	var layerReserva = new esri.layers.ArcGISDynamicMapServiceLayer(reservaUrl);
+	// nautical chart layer (S-57)
+	var nauticalChartLayer = new esri.layers.ArcGISDynamicMapServiceLayer(nauticalChartUrl);
 
-	// capa de barcos
-	dynamicLayer = new esri.layers.ArcGISDynamicMapServiceLayer(barcosUrl);
-	dynamicLayer.setDisableClientCaching(true);
+	// protected areas layer
+	var protectedAreasLayer = new esri.layers.ArcGISDynamicMapServiceLayer(protectedAreasUrl);
 
+	// ships layer
+	shipsLayer = new esri.layers.ArcGISDynamicMapServiceLayer(shipsUrl);
+	shipsLayer.setDisableClientCaching(true);
+
+	// feture layer to show popups
 	var template = new esri.InfoTemplate();
 	template.setTitle("<img src='http://79.125.13.101/flags/${COUNTRY:toLower}.png'/> <b>${NAME}</b>");
 	template.setContent("<img class='thumbnail' width='150px' height='112px' src='${THUMBNAIL}' onerror='this.src=\"noimage.png\"'/><br/><b>Course: </b>${COURSE}º<br /><b>Speed:</b> ${SPEED:divideByTen} kt")
@@ -90,10 +67,9 @@ function initMap()
 	map.setInfoWindow(infoWindowLite);
 	map.infoWindow.resize(155, 190);
 
-	layerBarcos = new esri.layers.FeatureLayer(barcosFeatureUrl + "/0",
+	shipsFeatureLayer = new esri.layers.FeatureLayer(shipsFeatureUrl + "/0",
 	{
 		mode: esri.layers.FeatureLayer.MODE_ONDEMAND,
-		//mode: esri.layers.FeatureLayer.MODE_SELECTION,
 		outFields: ["*"],
 		infoTemplate:template
 	});
@@ -102,9 +78,9 @@ function initMap()
 	symbol.setSize(30);
 	symbol.setColor(new dojo.Color([255,255,255,0]));
 	symbol.setOutline(new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_NULL));
-	layerBarcos.setRenderer(new esri.renderer.SimpleRenderer(symbol));
+	shipsFeatureLayer.setRenderer(new esri.renderer.SimpleRenderer(symbol));
 
-	dojo.connect(layerBarcos,"onMouseOver", function(evt)
+	dojo.connect(shipsFeatureLayer,"onMouseOver", function(evt)
 	{
 		//if( map.getScale() < 600000 )
 		{		
@@ -114,28 +90,26 @@ function initMap()
 			map.infoWindow.show( evt.screenPoint, map.getInfoWindowAnchor(evt.screenPoint));
 		}
 	});
-	dojo.connect(layerBarcos,"onMouseOut", function(evt)
+	dojo.connect(shipsFeatureLayer,"onMouseOut", function(evt)
 	{
 		map.infoWindow.hide();
 	})
 
-	map.addLayers([layerCartaNautica,layerReserva,dynamicLayer,layerBarcos]);
+	map.addLayers([nauticalChartLayer,protectedAreasLayer,shipsLayer,shipsFeatureLayer]);
 
-	// para medir el tiempo
+
+	// -- measure refresh time
 	var updateStartTime;
-	dojo.connect(dynamicLayer,"onUpdateStart", function()
+	dojo.connect(shipsLayer,"onUpdateStart", function()
 	{
 		updateStartTime = new Date().getTime();
-		//dojo.byId('elapsed').innerHTML = "<b>Tiempo:</b> - sec.";
 		dojo.byId('elapsed').innerHTML = "***";
 		waiting = true;
 	});
-	dojo.connect(dynamicLayer,"onUpdateEnd", function()
+	dojo.connect(shipsLayer,"onUpdateEnd", function()
 	{
 		var elapsed = new Date().getTime() - updateStartTime;
-		//console.log("elapsed: " + elapsed/1000 + " sec.");
 		dojo.byId('elapsed').innerHTML = "<b>Refresh:</b> " + elapsed/1000 + " sec.";
-		//dojo.byId('elapsed').innerHTML = "";
 		waiting = false;
 
 		if( playing )
@@ -144,14 +118,9 @@ function initMap()
 		}		
 	})
 
+	// -- update statistics after moving map
 	dojo.connect(map,'onPanEnd',updateStats);
 	dojo.connect(map,'onZoomEnd',updateStats);
-	// dojo.connect(map,'onClick', function(evt)
-	// {
-	// 	var query = new esri.tasks.Query();
-	// 	query.geometry = evt.mapPoint;
-	// 	layerBarcos.selectFeatures(query,esri.layers.FeatureLayer.SELECTION_NEW);
-	// });
 
 	updateStats();
 	play();
@@ -175,12 +144,12 @@ function stop()
 
 function updateStats()
 {
-	console.log("updating stats");
-
-	var statsTask = new esri.tasks.QueryTask(barcosUrl + "/0");
+	var statsTask = new esri.tasks.QueryTask(shipsUrl + "/0");
 	var query = new esri.tasks.Query();
 	var statsDef1 = new esri.tasks.StatisticDefinition();
 
+	// not useful, because this field gest updated with current timestamp instead of event timestamp
+	// see NIM090719: When working with time enabled data, the Date field attribute is automatically updated while updating a field in a feature service.
 	statsDef1.statisticType = "max";
 	statsDef1.onStatisticField = "TIMESTAMP_";
 	statsDef1.outStatisticFieldName = "maxTimestamp";
@@ -190,38 +159,26 @@ function updateStats()
 	statsDef2.onStatisticField = "MMSI";
 	statsDef2.outStatisticFieldName = "shipCount";
 
-	query.returnGeometry = false;
-	query.outStatistics = [statsDef1,statsDef2];
+	query.returnGeometry			 = false;
+	query.outStatistics 			 = [statsDef1,statsDef2];
 	query.groupByFieldsForStatistics = ["COUNTRY"];
-	query.orderByFields = ["shipCount DESC"];
-	query.geometry = map.extent;
+	query.orderByFields 			 = ["shipCount DESC"];
+	query.geometry 					 = map.extent;
 
 	statsTask.execute(query, 
 		function(result)
 		{
-			/*
-			var maxTimestamp = result.features[0].attributes.maxTimestamp;
-			var shipCount = result.features[0].attributes.shipCount;
-
-			if(maxTimestamp) {
-				maxTimestamp = dojo.date.locale.format(new Date(maxTimestamp));
-			} else {
-				maxTimestamp = "n/a";
-			}
-
-			console.log(maxTimestamp,shipCount);
-			*/
-// no lo muestro porque hay un NIM en los feature services que machacan el valor del campo timestamp con la hora actual
-//			dojo.byId('timestamp').innerHTML = "<b>Última Actualización</b><br/>" + maxTimestamp;
 			var countryCounts = "";
-			var total = 0;
+			var totalCount = 0;
 			result.features.forEach(function(feature) {
 				var imgUrl = "http://79.125.13.101/flags/" + feature.attributes.COUNTRY.toLowerCase() + ".png";
 				var img = "<img src='"+imgUrl+"' />";
 				countryCounts += "<li>" + img + " " + feature.attributes.shipCount + "</li>";
-				total += feature.attributes.shipCount;
+				totalCount += feature.attributes.shipCount;
 			});
-			dojo.byId('shipcount').innerHTML = "<b>Vessel count: </b>" + total + "<br/><ul>" + countryCounts + "</ul>";
+			dojo.byId('shipcount').innerHTML = 
+				"<b>Vessel count: </b>" + totalCount +
+				"<br/><ul>" + countryCounts + "</ul>";
 		},
 		function(error)
 		{
@@ -234,7 +191,7 @@ function refresh()
 {
 	if( ! waiting )
 	{
-		dynamicLayer.refresh();
+		shipsLayer.refresh();
 	}
 	updateStats();
 }
@@ -243,7 +200,7 @@ function clearHistory()
 {
 	[0,1].forEach(function(layerId)
 	{
-		var deleteUrl = barcosFeatureUrl + '/' + layerId + '/deleteFeatures';
+		var deleteUrl = shipsFeatureUrl + '/' + layerId + '/deleteFeatures';
 		console.log("deleting: " + deleteUrl);
 		var deleteRequest = esri.request({
 			url: deleteUrl,
@@ -270,12 +227,12 @@ function clearHistory()
 
 function showHistory()
 {
-	dynamicLayer.setVisibleLayers([0,1]);
+	shipsLayer.setVisibleLayers([0,1]);
 }
 
 function hideHistory()
 {
-	dynamicLayer.setVisibleLayers([0]);
+	shipsLayer.setVisibleLayers([0]);
 }
 
 dojo.addOnLoad(init);
